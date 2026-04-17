371286a102f3013da286a8d3a8d5464389420421
shade2105 <102245384+shade2105@users.noreply.github.com>
2026-04-17 19:14:56 +0300

    WinForms project

---
+++ WinForms/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace lab1_wf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(500, 400);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(71, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 10, 0, 10);
+            this.label1.Size = new System.Drawing.Size(358, 69);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "#Question_Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 16F);
+            this.label2.Location = new System.Drawing.Point(169, 69);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
+            this.label2.Size = new System.Drawing.Size(162, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "#Question_Text";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(115, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(200, 164);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "#Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 424);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WinForms/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Xml.Linq;
+using static lab1_wf.Form1;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace lab1_wf
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Question> _questions;
+        private List<string> _answers = new List<string>();
+        private bool _started = false;
+        private int _index = 0;
+        private int size = 0;
+        private string username;
+        private string title;
+
+        public class Question
+        {
+            public int Number { get; set; }
+            public string Text { get; set; }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = !string.IsNullOrWhiteSpace(textBox1.Text);
+        }
+
+
+        public void checkName()
+        {
+            label1.Text = title;			// назва форми опитування.
+            label2.Text = "Введіть ім'я";	// текст питання.
+            textBox1.Text = "";				// очищення текстового поля.
+            button1.Text = "Почати";			// зміна тексту кнопки.
+        }
+
+        public void StartSurvey(List<Question> list) // початок опитування.
+        {
+            label1.Anchor = AnchorStyles.Left;
+            label2.Anchor = AnchorStyles.Left;
+            label1.Margin = new Padding(15, 10, 0, 10);
+            label2.Margin = new Padding(20, 0, 0, 10);
+
+            textBox1.Margin = new Padding(25, 0, 0, 10);
+            textBox1.AcceptsReturn = true;
+            textBox1.WordWrap = true;
+            textBox1.Anchor = AnchorStyles.Left;
+            textBox1.Multiline = true;
+            textBox1.Dock = DockStyle.None;
+
+            tableLayoutPanel1.RowStyles[2].SizeType = SizeType.Absolute;
+            tableLayoutPanel1.RowStyles[2].Height = 80;
+            textBox1.Height = 80;
+            button1.Anchor = AnchorStyles.Left | AnchorStyles.Top;
+            button1.Margin = new Padding(25, 0, 0, 10);
+
+            _index = 0;
+            ShowQuestion();	// показ питань.
+        }
+
+        private void ShowQuestion()
+        {
+            if (_index >= size)	// перевірка на кінець опитування
+            {
+                textBox1.Visible = false;
+                tableLayoutPanel1.RowStyles[2].Height = 0;
+                label1.Visible = false;
+                label2.Text = "Готово!";
+                label2.Font = new Font(textBox1.Font.FontFamily, 24);
+                label2.Anchor = AnchorStyles.None;
+                label2.Margin = new Padding(5, 100, 0, 0);
+                button1.Text = "Вийти";
+                button1.Margin = new Padding(0, 10, 0, 0);
+                button1.Anchor = AnchorStyles.Top;
+                return;
+            }
+
+            var q = _questions[_index];	// отримання питання за індексом
+
+            label1.Text = $"Питання {q.Number}/{size}"; // номер питання.
+            label2.Text = q.Text;     // текст питання.
+            textBox1.Text = "";
+            button1.Text = "Далі";
+        }
+
+        private void SaveAnswers()	// зберігання відповідей у файл.
+        {
+            string folder = "Answers";
+
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+
+            //перевірка існуючих відповідей
+            var files = Directory.GetFiles(folder, "Answers*.xml");
+
+            int maxIndex = 0;
+            // цикл для знаходження останнього індексу Answers{n}.xml
+            foreach (var file in files)
+            {
+                string name = System.IO.Path.GetFileNameWithoutExtension(file); // Answer
+                string numberPart = new string(name.Where(char.IsDigit).ToArray());
+
+                if (int.TryParse(numberPart, out int n))
+                {
+                    if (n > maxIndex)
+                        maxIndex = n;
+                }
+            }
+
+
+            XDocument doc = new XDocument(
+                new XElement("Results",
+                    new XAttribute("date", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")),
+                    new XAttribute("username", username),
+
+                    _answers.Select((a, i) =>
+                        new XElement("Answer",
+                            new XAttribute("Question", _questions[i].Text),
+                            a
+                        )
+                    )
+                )
+            );
+
+            doc.Save($"Answers/Answers{maxIndex + 1}.xml");
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.TextChanged += textBox1_TextChanged;
+            this.BackColor = ColorTranslator.FromHtml("#BFBCCB");
+            tableLayoutPanel1.BackColor = ColorTranslator.FromHtml("#F4F3F6");
+
+            string path = "Forms/Form1.xml";
+            // читання xml в title та questions.
+            try
+            {
+                XDocument doc = XDocument.Load(path);
+                title = (string)doc.Root.Attribute("title");
+                List<Question> questions = doc.Root
+                    .Elements("Question")
+                    .Select(q => new Question
+                    {
+                        Number = (int)q.Attribute("number"),
+                        Text = (string)q.Element("Text")
+                    })
+                    .ToList();
+                _questions = questions;
+                size = _questions.Count;	// кількість питань
+                checkName();		// перейти до запиту імені.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при завантаженні файлу: {ex.Message}");
+                //Application.Current.Shutdown();
+                return;
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!_started)	// якщо опитування не почалося, записати ім’я та почати.
+            {
+                username = textBox1.Text;
+                _started = true;
+                StartSurvey(_questions);
+                return;
+            }
+            // якщо опитування завершено, зберегти та вийти.
+            if (_index >= _questions.Count)
+            {
+                SaveAnswers();
+                this.Close();
+                return;
+            }
+            // запис відповідей у змінну для подальшого запису в файл.
+            _answers.Add(textBox1.Text);
+            _index++;
+            ShowQuestion();
+        }
+    }
+}
